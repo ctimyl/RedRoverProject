1f34bfe351c0638ebbaa073c7dfee6d972459a5c
Artem Solovev <Solovyov@instories.com>
2023-10-09 19:56:43 +0200

    add Faker class

---
+++ Fake_user.py
@@ -0,0 +1,8 @@
+from faker import Faker
+class FakeUserGenerator:
+    # Генерация случайных пользовательских данных
+    def __init__(self):
+        self.fake = Faker()
+        self.first_name = self.fake.first_name()
+        self.last_name = self.fake.last_name()
+        self.postcode = self.fake.postcode()
