8a536a467b9ac0a3e845242e9a62cc4003abcb98
Artem Solovev <Solovyov@instories.com>
2023-10-09 19:56:21 +0200

    add tests

--- test_site.py
+++ test_site.py
@@ -3,19 +3,24 @@ from selenium import webdriver
 from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 import time
+from Fake_user import FakeUserGenerator
+
 
 BASE_URL = "https://www.saucedemo.com/"
-driver = webdriver.Chrome()
+
+def generate_fake_user():
+    # Генерация пользовательских данных
+    generated_user = FakeUserGenerator()
+    return generated_user
 
 @pytest.fixture()
 def successful_signup():
     driver = webdriver.Chrome()
+    driver.get(BASE_URL)
     # успешная авторизация
 
     login = "standard_user"
     password = "secret_sauce"
-
-    driver.get(BASE_URL)
 
     username_field = driver.find_element(By.XPATH, '//input[@data-test="username"]')
     username_field.send_keys(login)
@@ -35,13 +40,15 @@ def successful_signup():
 class TestAuth:
     def test_successful_signup(self, successful_signup):
         driver = successful_signup
+
         # Авторизация используя корректные данные (standard_user, secret_sauce)
         time.sleep(3)
         assert driver.current_url == "https://www.saucedemo.com/inventory.html"
 
     def test_unsuccessful_signup(self):
-        # Авторизация используя некорректные данные (user, user)
+        driver = webdriver.Chrome()
         driver.get(BASE_URL)
+        # Авторизация используя некорректные данные (user, user)
         username_field = driver.find_element(By.XPATH, '//input[@data-test="username"]')
         username_field.send_keys("user")
 
@@ -54,6 +61,7 @@ class TestAuth:
         time.sleep(2)
         error_button = driver.find_element(By.XPATH, '//button[@class="error-button"]')
         assert error_button.is_displayed()
+        driver.quit()
 
 
 class TestBasket:
@@ -71,7 +79,8 @@ class TestBasket:
         basket_badge = driver.find_element(By.XPATH, '//*[@id="shopping_cart_container"]/a')
         assert basket_badge.text == '1'
 
-    def test_add_from_basket(self, successful_signup):
+    def test_del_from_basket(self, successful_signup):
+        # Удаление товара из корзины через корзину
         driver = successful_signup
 
         add_basket_button = driver.find_element(By.XPATH, '//button[starts-with(@id, "add-to-cart-sauce-labs-")]')
@@ -137,6 +146,7 @@ class TestBasket:
 
 class TestCardProcuct:
     def test_move_from_img(self, successful_signup):
+        # Успешный переход к карточке товара после клика на картинку товара
         driver = successful_signup
 
         expected_id = '4'  # id нужного нам товара
@@ -149,6 +159,7 @@ class TestCardProcuct:
 
 
     def test_move_from_title(self, successful_signup):
+        # Успешный переход к карточке товара после клика на название товара
         driver = successful_signup
 
         expected_id = '4'  # id нужного нам товара
@@ -158,3 +169,37 @@ class TestCardProcuct:
         time.sleep(1)
 
         assert driver.current_url == f'https://www.saucedemo.com/inventory-item.html?id={expected_id}'
+
+class TestOrder:
+    def test_do_ordering(self, successful_signup):
+        # Оформление заказа используя корректные данные
+        driver = successful_signup
+        generated_user = generate_fake_user()
+
+        add_basket_button = driver.find_element(By.XPATH, '//button[starts-with(@id, "add-to-cart-sauce-labs-")]')
+        add_basket_button.click()
+
+        basket_button = driver.find_element(By.XPATH, '//*[@id="shopping_cart_container"]/a')
+        basket_button.click()
+
+        checkout_button = driver.find_element(By.XPATH, '//*[@id="checkout"]')
+        checkout_button.click()
+
+        first_name_field = driver.find_element(By.XPATH, '//*[@id="first-name"]')
+        first_name_field.send_keys(generated_user.first_name)
+
+        last_name_field = driver.find_element(By.XPATH, '//*[@id="last-name"]')
+        last_name_field.send_keys(generated_user.last_name)
+
+        postcode_field = driver.find_element(By.XPATH, '//*[@id="postal-code"]')
+        postcode_field.send_keys(generated_user.postcode)
+
+        continue_button = driver.find_element(By.XPATH, '//*[@id="continue"]')
+        continue_button.click()
+
+        finish_button = driver.find_element(By.XPATH, '//*[@id="finish"]')
+        finish_button.click()
+
+        back_home_button = driver.find_element(By.XPATH, '//*[@id="back-to-products"]')
+
+        assert back_home_button.is_displayed()
