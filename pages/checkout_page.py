bb25247e61efb95b222d9139ed19af6ca51f439c
Artem Solovev <Solovyov@instories.com>
2023-10-17 20:01:48 +0200

    переделал локаторы на айди

--- pages/checkout_page.py
+++ pages/checkout_page.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 class CheckoutPage:
     FIRST_NAME_FIELD = (By.ID, 'first-name')
     LAST_NAME_FIELD = (By.ID, 'last-name')
-    POSTCODE_FIELD = (By.ID, 'id="postal-code')
+    POSTCODE_FIELD = (By.ID, 'postal-code')
     CONTINUE_BUTTON = (By.ID, 'continue')
     CARD_TITLE = (By.XPATH, '//*[@id="item_4_title_link"]/div')
     CANCEL_BUTTON = (By.ID, 'cancel')
