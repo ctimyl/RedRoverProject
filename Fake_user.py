2dbaa56db8eb689d7a69665f60de82e0bb6a5073
Artem Solovev <Solovyov@instories.com>
2023-10-17 14:00:21 +0200

    подготовил всё для написания тестов

--- Fake_user.py
+++ Fake_user.py
@@ -1,4 +1,6 @@
 from faker import Faker
+
+
 class FakeUserGenerator:
     # Генерация случайных пользовательских данных
     def __init__(self):
@@ -6,3 +8,8 @@ class FakeUserGenerator:
         self.first_name = self.fake.first_name()
         self.last_name = self.fake.last_name()
         self.postcode = self.fake.postcode()
+
+def generate_fake_user():
+    # Генерация пользовательских данных
+    generated_user = FakeUserGenerator()
+    return generated_user
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,5 +1,6 @@
 from steps.login_steps import LoginSteps
 from steps.catalog_steps import CatalogSteps
+from Fake_user import generate_fake_user
 
 class TestLogin:
 
@@ -13,3 +14,10 @@ class TestLogin:
         login_steps.click_login()
         catalog_steps = CatalogSteps(browser)
         catalog_steps.catalog_page_should_be_displayed()
+
+    # def fake(self, browser):
+    #     first_name_field = LoginSteps(browser)
+    #     first_name_field.click_login()
+    #     do_fake_user = generate_fake_user()
+    #     name = do_fake_user.first_name
+    #
